97915617ee2ef4ebafd0bf8027e4c2ea756924fa
thanyawzinmin <thanyawzinmin91@gmail.com>
2013-10-25 18:02:37 +0630

    API Speakers controller added

---
+++ app/controllers/api/v1/speakers_controller.rb
@@ -0,0 +1,12 @@
+module Api
+  module V1
+    class SpeakersController < ApplicationController
+      respond_to :json
+
+      def index
+        @speakers = Speaker.all
+      end
+
+    end
+  end
+end
