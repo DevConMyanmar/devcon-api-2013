4d1db8b06db2365e70dde937ac760b387cc77e22
thanyawzinmin <thanyawzinmin91@gmail.com>
2013-10-25 17:47:00 +0630

    Should be , defaults:

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Devconapi::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  namespace :api defaults: { :format => 'json' } do
+  namespace :api, defaults: { :format => 'json' } do
     namespace :v1 do
       resources :schedules
     end
