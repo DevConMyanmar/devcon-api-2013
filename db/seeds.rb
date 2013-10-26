e6cd300ef20af5cb2a41d1d6eb615632d5ed9f6a
thanyawzinmin <thanyawzinmin91@gmail.com>
2013-10-26 08:48:30 +0630

    Added two speakers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,13 @@
 Speaker.create!([
-  name: "Tin Aung Lin",
-  email: "tal@nexlabs.co",
-  bio: "Tin Aung Lin is a web developer at Nex, who is committed to create breathtaking web designs. He is always eager to expand his horizons by learning new things. Truly in his heart, Tin despises HTML, dynamic CSS, Scripting and Server  Architecture. He has 3 years experience in LAMP stack web development. At the present, Tin is learning Ruby on Rails programming language, which he takes as a new interest and tends to fully utilize in the near future. Before venturing into the field of programming, Tin was a student at Mandalay Technological University, pursuing his Bachelor in Civil Engineering. Later, he bravely quitted his educational path and jumped in with both feet into programming. Tin currently has a developer internship position at Nex. Every day, he puts tremendous effort into creating things that everyone will fall in love with.",
-  :photo: "http://devconmyanmar.org/2013/wp-content/uploads/2013/10/tinal-150x150.png"
+  name: "Ei Maung",
+  email: "",
+  bio: "Professional web developer since 2006. Founder at Fairway Web and currently providing web development and consultant services for many happy clients across the world. Also coaching and training newcomers with a web development course and already produced hundreds of professional web developers.",
+  photo: "http://devconmyanmar.org/2013/wp-content/uploads/2013/10/eimg.png"
 ])
 Speaker.create!([
-  name: "Ye Lin Aung"
-  email: "
-
+  name: "Thiha Aye Kyaw",
+  email: "",
+  bio:"Graduated IDCS from MCC and I’ve developed Frozen Keyboard for Android since 2011. I’ve been doing Android projects for more than 2 years and I’m only interested in Android.",
+  photo: "http://devconmyanmar.org/2013/wp-content/uploads/2013/10/thihaak.png"
+])
 puts "Seeding done"
