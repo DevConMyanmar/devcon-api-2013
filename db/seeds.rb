62ffdb4aa18d81376a6caefb8d9b74198b7db81a
thanyawzinmin <thanyawzinmin91@gmail.com>
2013-10-26 20:51:51 +0630

    talktype seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -237,5 +237,22 @@ His current interest topics are: Enterprise Architecture, Cloud Computing, Enter
 
 ])
 
+Talktype.create!( [
+  {
+    name: "Keynote"
+  },
+
+  {
+    name: "Web Development"
+  },
+
+  {
+    name: "Mobile Development"
+  },
+
+  {
+    name: "Computer Science"
+  },
+])
 # give out the message
 puts "Seeding done"
