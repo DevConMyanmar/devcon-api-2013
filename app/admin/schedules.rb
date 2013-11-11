f8403edad18b3f133ebdc4fdc097a82d7541696a
tal <tinaunglinn.eng91@gmail.com>
2013-11-11 17:51:51 +0630

    Active admin's form fixed

--- app/admin/schedules.rb
+++ app/admin/schedules.rb
@@ -8,7 +8,7 @@ ActiveAdmin.register Schedule do
       f.input :session_date,  as: :datepicker
       f.input :speaker_ids , collection: Speaker.all.map { |et| ["#{et.name}", et._id.to_s] }
       f.input :location_id , collection: Location.all.map { |et| ["#{et.name}", et._id.to_s] }
-      f.input :talktype_id, collection: Talktype.all.map { |et| ["#{et.name}" , et._id.to_s] }
+      f.input :talktype_ids, collection: Talktype.all.map { |et| ["#{et.name}" , et._id.to_s] }
     end
     f.actions
   end
